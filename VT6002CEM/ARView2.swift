51f8ae5ec797cbc6846cbf0b74b9302405b3b4d2
vincent <118966490+FUFU487@users.noreply.github.com>
2025-01-31 22:14:27 +0800

    Final version

--- VT6002CEM/ARView2.swift
+++ VT6002CEM/ARView2.swift
@@ -102,15 +102,22 @@ struct ARViewContainer: UIViewRepresentable {
         var frequencies = [Float](repeating: 0.0, count: sampleCount)
         
         for i in 0..<sampleCount {
-            frequencies[i] = sin(Float(i) * 0.1) * 0.5 + 0.5
+            frequencies[i] = sin(Float(i) * 0.1 + Float(Date().timeIntervalSince1970)) * (0.5 + Float.random(in: -0.2...0.2)) + 0.5
         }
         return frequencies
     }
     
     private func displayDynamicWaveform(in arView: ARSCNView, frequencies: [Float]) {
         DispatchQueue.main.async {
+            arView.scene.rootNode.enumerateChildNodes { (node, _) in
+                node.position.x -= 0.02 // 移動波形，使其由右向左移動
+                if node.position.x < -5.0 {
+                    node.removeFromParentNode()
+                }
+            }
+            
             let waveformNode = SCNNode()
-            waveformNode.position = SCNVector3(0, 0, -2)
+            waveformNode.position = SCNVector3(0, 0, -2) // 調整波形位置到正前方
             
             for (index, value) in frequencies.enumerated() {
                 let barWidth: Float = 0.02
@@ -123,7 +130,7 @@ struct ARViewContainer: UIViewRepresentable {
                 box.materials = [material]
                 
                 let boxNode = SCNNode(geometry: box)
-                boxNode.position = SCNVector3(Float(index) * barWidth - 5.0, Float(value) * 0.25, 0)
+                boxNode.position = SCNVector3(Float(index) * barWidth - 5.0, Float(value) * 0.25, 0) // 調整位置，使波形顯示在正前方
                 waveformNode.addChildNode(boxNode)
             }
             
